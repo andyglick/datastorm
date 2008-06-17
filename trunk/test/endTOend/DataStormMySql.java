df0c15313780fda9085a26f6b9a0a8653652881e
kbg <kbg@4f184ae1-c8e1-4fd5-973e-467c5d099c44>
2008-06-17 16:05:55 +0000

    added empty sql call

--- trunk/test/endTOend/DataStormMySql.java
+++ trunk/test/endTOend/DataStormMySql.java
@@ -3,10 +3,16 @@ import java.sql.DriverManager;
 
 import org.datastorm.DataStorm;
 
+/**
+ * Simple roundtrip test connecting to a mysql db
+ * 
+ * @author Kasper B. Graversen, (c) 2007-2008
+ */
 public class DataStormMySql {
 public static void main(String[] args) throws Exception {
 	Class.forName("com.mysql.jdbc.Driver").newInstance();
 	Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/mysql", "root", "admin");
+	new DataStorm().show(conn);
 	new DataStorm().show(conn, "SELECT * FROM help_topic h;");
 }
 
