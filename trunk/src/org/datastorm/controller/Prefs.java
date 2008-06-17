38f2a63a5fe9f8ffe71336aed0b406c5e6b22c4c
kbg <kbg@4f184ae1-c8e1-4fd5-973e-467c5d099c44>
2008-06-17 16:07:31 +0000

    added query restriction and empty sql does not error out

--- trunk/src/org/datastorm/controller/Prefs.java
+++ trunk/src/org/datastorm/controller/Prefs.java
@@ -3,5 +3,7 @@ package org.datastorm.controller;
 public class Prefs {
 public static final int WINDOW_X = 600;
 public static final int WINDOW_Y = 500;
-public static final String VERSION = "v1.0.0 Beta";
+public static final String VERSION = "v1.0.0 Beta 2";
+public static final int MAX_RESULTSET_SIZE = 500;
+
 }
